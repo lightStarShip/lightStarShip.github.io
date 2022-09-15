3faf8f383a646e274d2fa4422d9bd10efbd33e68
hopwesley <ws@sinoc.io>
2022-09-15 18:37:38 +0800

    new node

--- ruleVer.js
+++ ruleVer.js
@@ -1,1 +1,1 @@
-{"dns":13,"by_pass":1,"must_hit":1,"config":16,"price":10, "dollar":"cny", "macVer":10007}
+{"dns":13,"by_pass":1,"must_hit":1,"config":17,"price":10, "dollar":"cny", "macVer":10007}
