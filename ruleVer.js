1210fc7edfb38ba65c5d19520b8412d635c8a9a4
hopwesley <ws@sinoc.io>
2022-09-15 16:07:31 +0800

    version build 7 for mac os

--- ruleVer.js
+++ ruleVer.js
@@ -1,1 +1,1 @@
-{"dns":13,"by_pass":1,"must_hit":1,"config":15,"price":10, "dollar":"cny", "macVer":10006}
+{"dns":13,"by_pass":1,"must_hit":1,"config":15,"price":10, "dollar":"cny", "macVer":10007}
