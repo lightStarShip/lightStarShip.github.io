4cb3948c20e664aad980cde9e45c9f192367d562
hopwesley <ws@sinoc.io>
2022-09-16 11:38:58 +0800

    new mac version

--- ruleVer.js
+++ ruleVer.js
@@ -1,1 +1,1 @@
-{"dns":14,"by_pass":1,"must_hit":1,"config":18,"price":10, "dollar":"cny", "macVer":10008}
+{"dns":14,"by_pass":1,"must_hit":1,"config":18,"price":10, "dollar":"cny", "macVer":10009}
