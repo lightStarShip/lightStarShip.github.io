3e5145863bf5bd03955e14f4686bd761970eb035
hopwesley <ws@sinoc.io>
2022-09-16 13:08:07 +0800

    new free node

--- ruleVer.js
+++ ruleVer.js
@@ -1,1 +1,1 @@
-{"dns":14,"by_pass":1,"must_hit":1,"config":19,"price":10, "dollar":"cny", "macVer":10009}
+{"dns":14,"by_pass":1,"must_hit":1,"config":20,"price":10, "dollar":"cny", "macVer":10009}
