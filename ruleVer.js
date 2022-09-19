dcdb7c1f023b9b4279fa8165c9674406557e6ee9
hopwesley <ws@sinoc.io>
2022-09-19 15:56:11 +0800

    change wallet for node 49

--- ruleVer.js
+++ ruleVer.js
@@ -1,1 +1,1 @@
-{"dns":14,"by_pass":1,"must_hit":1,"config":20,"price":10, "dollar":"cny", "macVer":10010}
+{"dns":14,"by_pass":1,"must_hit":1,"config":21,"price":10, "dollar":"cny", "macVer":10010}
