f3967a1cfe69d413f8ef098571a5e000d15b0159
hopwesley <ws@sinoc.io>
2022-10-19 12:38:40 +0800

    dns update

--- ruleVer.js
+++ ruleVer.js
@@ -1,1 +1,1 @@
-{"dns":14,"by_pass":1,"must_hit":1,"config":25,"price":10, "dollar":"cny", "macVer":10011}
+{"dns":15,"by_pass":1,"must_hit":1,"config":25,"price":10, "dollar":"cny", "macVer":10011}
