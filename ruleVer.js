614217f130d6821e33076dbfe6dc982c17465f14
hopwesley <ws@sinoc.io>
2022-11-11 11:28:42 +0800

    nodes

--- ruleVer.js
+++ ruleVer.js
@@ -1,1 +1,1 @@
-{"dns":21,"by_pass":1,"must_hit":1,"config":27,"price":10, "dollar":"cny", "macVer":10011}
+{"dns":21,"by_pass":1,"must_hit":1,"config":28,"price":10, "dollar":"cny", "macVer":10011}
