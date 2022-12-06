2ded9860e08c4ade4aaf5e597ae552cbcac5b794
hopwesley <ws@sinoc.io>
2022-12-06 21:12:26 +0800

    new app version

--- ruleVer.js
+++ ruleVer.js
@@ -1,1 +1,1 @@
-{"dns":21,"by_pass":1,"must_hit":1,"config":28,"price":10, "dollar":"cny", "macVer":10013}
+{"dns":21,"by_pass":1,"must_hit":1,"config":28,"price":10, "dollar":"cny", "macVer":10014}
