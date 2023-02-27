580d35dd4ac60b4768d23235cd6b24bb17dd511f
basdevelop <hyperorchid@outlook.com>
2023-02-27 11:50:44 +0800

    node changed

--- ruleVer.js
+++ ruleVer.js
@@ -1,1 +1,1 @@
-{"dns":21,"by_pass":1,"must_hit":1,"config":28,"price":10, "dollar":"cny", "macVer":10014}
+{"dns":21,"by_pass":1,"must_hit":1,"config":29,"price":10, "dollar":"cny", "macVer":10014}
