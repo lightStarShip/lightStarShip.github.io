e57e944ee09cc085d184beef67ce846b480ae816
alen-x <alen-x@tutanota.com>
2023-04-17 08:18:18 +0800

    Add new field

--- version.js
+++ version.js
@@ -1,1 +1,1 @@
-{"Ver":10018,"VerName":"1.1.8","Url":"","Des":["1.暂对大陆用户停止服务"]}
+{"Ver":10018,"VerName":"1.1.8","Url":"","Des":["1.暂对大陆用户停止服务"],"NewVer":10000,"NewVerName":"1.00.00","NewDes":[""],"NewUrl":"http://config.simplenets.org:8888/star.apk"}
