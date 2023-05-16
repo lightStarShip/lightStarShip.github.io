1f76c9b11ec26378925c7cff24dfaf1c1b6586b0
basdevelop <hyperorchid@outlook.com>
2023-05-16 10:21:18 +0800

    new rule

--- ruleVer.js
+++ ruleVer.js
@@ -1,1 +1,1 @@
-{"dns":22,"by_pass":1,"must_hit":1,"config":35,"price":10, "dollar":"cny", "macVer":10014}
+{"dns":23,"by_pass":1,"must_hit":1,"config":35,"price":10, "dollar":"cny", "macVer":10014}
