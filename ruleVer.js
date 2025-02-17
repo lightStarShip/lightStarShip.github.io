903efcfbe85d6e7af664be823b00c4d96eb924a3
basdevelop <hyperorchid@outlook.com>
2025-02-17 14:11:01 +0800

    free the 252 node

--- ruleVer.js
+++ ruleVer.js
@@ -1,1 +1,1 @@
-{"dns":31,"by_pass":2,"must_hit":2,"config":41,"price":10, "dollar":"cny", "macVer":10014}
+{"dns":32,"by_pass":2,"must_hit":2,"config":42,"price":10, "dollar":"cny", "macVer":10014}
