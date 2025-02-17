fbe55c5e2b9ea49eb39da45aa397de4147ec3f12
basdevelop <hyperorchid@outlook.com>
2025-02-17 15:02:56 +0800

    remove the 22 node

--- ruleVer.js
+++ ruleVer.js
@@ -1,1 +1,1 @@
-{"dns":33,"by_pass":3,"must_hit":3,"config":44,"price":10, "dollar":"cny", "macVer":10014}
+{"dns":34,"by_pass":3,"must_hit":3,"config":45,"price":10, "dollar":"cny", "macVer":10014}
