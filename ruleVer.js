927afb303f7de23591886b24266e6af062406947
basdevelop <hyperorchid@outlook.com>
2025-02-25 14:15:34 +0800

    version update

--- ruleVer.js
+++ ruleVer.js
@@ -1,1 +1,1 @@
-{"dns":34,"by_pass":3,"must_hit":3,"config":45,"price":10, "dollar":"cny", "macVer":10014}
+{"dns":35,"by_pass":3,"must_hit":3,"config":46,"price":10, "dollar":"cny", "macVer":10014}
