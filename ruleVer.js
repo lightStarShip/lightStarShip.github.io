db9a09470b5301dc407bba15429781739281d67a
basdevelop <hyperorchid@outlook.com>
2025-10-05 12:10:03 +0800

    new dns

--- ruleVer.js
+++ ruleVer.js
@@ -1,1 +1,1 @@
-{"dns":38,"by_pass":3,"must_hit":3,"config":49,"price":10, "dollar":"cny", "macVer":10015}
+{"dns":39,"by_pass":3,"must_hit":3,"config":50,"price":10, "dollar":"cny", "macVer":10015}
