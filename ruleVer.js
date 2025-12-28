571e02fd241f526a277eb9df460041235dd6310b
basdevelop <hyperorchid@outlook.com>
2025-12-28 18:00:40 +0800

    update dns record

--- ruleVer.js
+++ ruleVer.js
@@ -1,1 +1,1 @@
-{"dns":40,"by_pass":3,"must_hit":3,"config":51,"price":10, "dollar":"cny", "macVer":10015}
+{"dns":41,"by_pass":3,"must_hit":3,"config":52,"price":10, "dollar":"cny", "macVer":10015}
