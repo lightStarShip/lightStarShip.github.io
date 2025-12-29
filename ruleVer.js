12c98ad9bee921c301118d51cafec09365bbab6d
basdevelop <hyperorchid@outlook.com>
2025-12-29 11:34:27 +0800

    role update

--- ruleVer.js
+++ ruleVer.js
@@ -1,1 +1,1 @@
-{"dns":41,"by_pass":3,"must_hit":3,"config":52,"price":10, "dollar":"cny", "macVer":10015}
+{"dns":42,"by_pass":3,"must_hit":3,"config":53,"price":10, "dollar":"cny", "macVer":10015}
